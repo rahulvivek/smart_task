5e7f773369e8cdc97987e665f2171a8374c1b188
Rahul Vivek <rahul@cognicor.com>
2018-11-22 14:33:23 +0530

    Setting local settings

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smart_task.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smart_task.settings.local")
 
     from django.core.management import execute_from_command_line
 
